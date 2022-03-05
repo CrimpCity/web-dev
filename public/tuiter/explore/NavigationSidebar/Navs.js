2c608d526ffc38edfd5765cc3600caab4a314d7c
CrimpCity <tutuianu.georgian@gmail.com>
2022-03-04 23:02:18 -0500

    add elon tweet and some grimes

--- public/tuiter/explore/NavigationSidebar/Navs.js
+++ public/tuiter/explore/NavigationSidebar/Navs.js
@@ -3,49 +3,41 @@ export default [
         title: 'Home',
         link: 'tuiter/home.html',
         icon: '<i class="fas fa-home fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
     {
         title: 'Explore',
         link: 'tuiter/explore/explore.html',
         icon: '<i class="fas fa-hashtag fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
     {
         title: 'Notifications',
         link: 'tuiter/notifications.html',
         icon: '<i class="fas fa-bell fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
     {
         title: 'Messages',
         link: 'tuiter/messages.html',
         icon: '<i class="fas fa-envelope fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
     {
         title: 'Bookmarks',
         link: 'tuiter/bookmarks/bookmarks.html',
         icon: '<i class="fas fa-bookmark fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
     {
         title: 'Lists',
         link: 'tuiter/lists.html',
         icon: '<i class="fas fa-list fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
     {
         title: 'Profile',
         link: 'tuiter/profile.html',
         icon: '<i class="fas fa-user fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
     {
         title: 'More',
         link: "#",
         // More icon is no longer stacked with ellipsis and circle it's just a circle 
         icon: '<i class="fa-solid fas fa-circle fa-fw wd-right-pad-icon"></i>',
-        isActive: false,
     },
 ];
