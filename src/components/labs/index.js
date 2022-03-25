efb95776386e18108e58b397021c82ab44ea3ca8
CrimpCity <tutuianu.georgian@gmail.com>
2022-03-24 20:03:54 -0400

    add lab styles

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+
+// const Labs = () => {
+//     return (
+//         <>
+//             <h1>Labs</h1>
+//             <Link to="/hello">
+//                 Hello
+//             </Link> |
+//             <Link to="/tuiter">
+//                 Tuiter
+//             </Link>
+//         </>
+//     )
+// };
+
+// export default Labs;
+
+
+
 
 const Labs = () => {
     return (
-        <>
+        <div>
             <h1>Labs</h1>
-            <Link to="/hello">
-                Hello
-            </Link> |
-            <Link to="/tuiter">
-                Tuiter
-            </Link>
-        </>
+            <Styles />
+            <Classes />
+        </div>
     )
 };
-
 export default Labs;
