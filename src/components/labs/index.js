0b37faf66a6b5d49501bbaaa0c8ad0cfc474d82b
CrimpCity <tutuianu.georgian@gmail.com>
2022-03-27 17:38:28 -0400

    add links to landing page

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output/index.js";
@@ -27,6 +27,21 @@ const Labs = () => {
     return (
         <div>
             <h1>Labs</h1>
+            <Link to="/hello">
+                Hello
+            </Link>
+            |
+            <Link to="/tuiter/home">
+                Tuiter
+            </Link>
+            |
+            <Link to="/tuiter">
+                Tuiter A6
+            </Link>
+            |
+            <Link to="/home">
+                Home Screen A6
+            </Link>
             <TodoList />
             <ConditionalOutput />
             <Styles />
