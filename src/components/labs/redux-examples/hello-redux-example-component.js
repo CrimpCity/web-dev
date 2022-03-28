c4c0539fa5031614161664ab26a70fa4208f84b7
CrimpCity <tutuianu.georgian@gmail.com>
2022-03-28 15:40:39 -0400

    add todo lab redux examples

--- src/components/labs/redux-examples/hello-redux-example-component.js
+++ src/components/labs/redux-examples/hello-redux-example-component.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+// const HelloReduxExampleComponent = () => {
+//     // extract 'Hello World' from reducer
+//     const message = useSelector((hello) => hello.message);
+//     return (
+//         <h1>{message}</h1> // render <h1>Hello World</h1>
+//     );
+// };
+
+// export default HelloReduxExampleComponent;
+
+
+
+
 const HelloReduxExampleComponent = () => {
-    // extract 'Hello World' from reducer
-    const message = useSelector((hello) => hello.message);
+    const message = useSelector((state) => state.hello.message);
     return (
-        <h1>{message}</h1> // render <h1>Hello World</h1>
+        <h1>{message}</h1>
     );
 };
 
 export default HelloReduxExampleComponent;
-
-
-
-
-
-
 
 
