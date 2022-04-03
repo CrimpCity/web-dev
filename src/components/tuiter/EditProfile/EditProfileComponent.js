ef69213c85ed1467fc477f09770723378ea1b8d8
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-03 11:30:18 -0400

    add edit functionality challenge

--- src/components/tuiter/EditProfile/EditProfileComponent.js
+++ src/components/tuiter/EditProfile/EditProfileComponent.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./EditProfile.css"
 
-
-const EditProfileComponent = (profile) => {
+const EditProfileComponent = () => {
+    const currentProfile = useSelector((state) => state.profile);
+    let [profile, setProfile] = useState(currentProfile);
     const dispatch = useDispatch();
-    // const [profile, setProfile] = useState({ profile: 'profile' });
-    const deleteProfileChanges = () => { dispatch({ type: 'delete-profile-changes', profile }) }
-    // const nameChangeHandler = (event) => {
-    //     const newValue = event.target.value;
-    //     const newName = {
-    //         firstName: newValue
-    //     };
-    //     setProfile(newName);
-    // }
+    const saveProfileEdit = () => {
+        dispatch({
+            type: 'save-profile-edit',
+            profile: profile
+        })
+    };
+    const exitEdits = () => {
+        dispatch({
+            type: 'exit-profile-edit',
+        })
+    };
 
     const formatted = (
         <>
             <div className="row wd-text-color wd-container mb-2">
                 <div className="col-1 fs-6">
-                    <i onClick={() => deleteProfileChanges(profile)}
-                        className="fas fa-trash"></i>
+                    <Link to="/tuiter/profile">
+                        <i onClick={exitEdits} className="fas fa-trash wd-text-color"></i>
+                    </Link>
+
                 </div>
                 <div className="col-10">
                     <div className="fs-5 fw-bold">
@@ -31,7 +36,8 @@ const EditProfileComponent = (profile) => {
                 </div>
                 <div className="col-1">
                     <Link to="/tuiter/profile">
-                        <button className="btn btn-primary wd-save-button fw-bold float-end">
+                        <button className="btn btn-primary wd-save-button fw-bold float-end"
+                            onClick={saveProfileEdit}>
                             Save
                         </button>
                     </Link>
@@ -41,12 +47,12 @@ const EditProfileComponent = (profile) => {
 
             <div className="position-relative">
                 <img className=" wd-banner-image"
-                    src={profile.profile.bannerPicture}
+                    src={profile.bannerPicture}
                     alt="banner">
                 </img>
                 <div className="position-absolute wd-avatar-photo-position ps-3">
                     <img className="wd-avatar-photo"
-                        src={profile.profile.profilePicture}
+                        src={profile.profilePicture}
                         alt="avatar">
                     </img>
                 </div>
@@ -56,14 +62,28 @@ const EditProfileComponent = (profile) => {
 
                 <div className="wd-text-box-border wd-name-box-height">
                     <div className="mt-1">
-                        Name
+                        First Name
                     </div>
 
                     <div>
                         <textarea
-                            value={profile.profile.firstName + " " + profile.profile.lastName}
-                            className="wd-text-box wd-name-form-height">
-                            {profile.profile.firstName}
+                            placeholder={profile.firstName}
+                            className="wd-text-box wd-name-form-height"
+                            onChange={(event) => setProfile({ ...profile, firstName: event.target.value })}>
+                        </textarea>
+                    </div>
+                </div>
+
+                <div className="mt-4 wd-text-box-border wd-name-box-height">
+                    <div className="mt-1">
+                        Last Name
+                    </div>
+
+                    <div>
+                        <textarea
+                            placeholder={profile.lastName}
+                            className="wd-text-box wd-name-form-height"
+                            onChange={(event) => setProfile({ ...profile, lastName: event.target.value })}>
                         </textarea>
                     </div>
                 </div>
@@ -75,9 +95,9 @@ const EditProfileComponent = (profile) => {
 
                     <div>
                         <textarea
-                            value={profile.profile.bio}
-                            className="wd-text-box wd-bio-form-height">
-                            {profile.profile.bio}
+                            placeholder={profile.bio}
+                            className="wd-text-box wd-bio-form-height"
+                            onChange={(event) => setProfile({ ...profile, bio: event.target.value })}>
                         </textarea>
                     </div>
                 </div>
@@ -90,9 +110,9 @@ const EditProfileComponent = (profile) => {
 
                     <div>
                         <textarea
-                            value={profile.profile.location}
-                            className="wd-text-box wd-location-form-height">
-                            {profile.profile.location}
+                            placeholder={profile.location}
+                            className="wd-text-box wd-location-form-height"
+                            onChange={(event) => setProfile({ ...profile, location: event.target.value })}>
                         </textarea>
                     </div>
                 </div>
@@ -105,9 +125,9 @@ const EditProfileComponent = (profile) => {
 
                     <div>
                         <textarea
-                            value={profile.profile.website}
-                            className="wd-text-box wd-website-form-height">
-                            {profile.profile.website}
+                            placeholder={profile.website}
+                            className="wd-text-box wd-website-form-height"
+                            onChange={(event) => setProfile({ ...profile, website: event.target.value })}>
                         </textarea>
                     </div>
                 </div>
@@ -119,20 +139,12 @@ const EditProfileComponent = (profile) => {
 
                     <div>
                         <textarea
-                            value={profile.profile.dateOfBirth}
-                            className="wd-text-box wd-website-form-height">
-                            {profile.profile.dateOfBirth}
+                            placeholder={profile.dateOfBirth}
+                            className="wd-text-box wd-website-form-height"
+                            onChange={(event) => setProfile({ ...profile, dateOfBirth: event.target.value })}>
                         </textarea>
                     </div>
                 </div>
-
-
-
-
-
-
-
-
             </div>
         </>
     )
--- src/components/tuiter/reducers/profile-reducer.js
+++ src/components/tuiter/reducers/profile-reducer.js
@@ -1,7 +1,14 @@
 import profileJson from '../data/profile.json';
 
-const profileReducer = (state = profileJson) => {
-    return (state);
+const profileReducer = (state = profileJson, action) => {
+    switch (action.type) {
+        case "save-profile-edit":
+            return action.profile;
+        case "exit-profile-edit":
+            return state;
+        default:
+            return state
+    }
 };
 
 export default profileReducer;
